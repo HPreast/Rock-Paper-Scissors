50f231af3d1a3bc8eee17ec86f3d9395ec48eb95
HPreast <preasths1@gmail.com>
2021-05-25 15:19:10 -0500

    generated user throws

--- Program.cs
+++ Program.cs
@@ -1,12 +1,53 @@
 ﻿using System;
 
-namespace Rock_Paper_Scissors
+Console.WriteLine("-------------------------");
+Console.WriteLine("| Player:  | Computer:  |");
+Console.WriteLine("-------------------------");
+
+Console.WriteLine("What would you like to throw?");
+Console.WriteLine("1) Rock");
+Console.WriteLine("2) Paper");
+Console.WriteLine("3) Scissors");
+
+string userThrowToConvert = Console.ReadLine();
+int userThrow = Convert.ToInt32(userThrowToConvert);
+int compThrow = new Random().Next(1, 3);
+
+userThrows();
+
+void userThrows()
 {
-    class Program
+    if (userThrow == 1)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        Console.WriteLine(@"""
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+""");
+    }
+    else if (userThrow == 2)
+    {
+        Console.WriteLine(@"""
+     _______
+---'    ____)____
+           ______)
+          _______)
+         _______)
+---.__________)
+""");
+    }
+    else if (userThrow == 3)
+    {
+        Console.WriteLine(@"""
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+""");
     }
 }
